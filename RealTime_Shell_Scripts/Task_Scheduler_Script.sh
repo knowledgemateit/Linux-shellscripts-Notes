5e57615c6fca00053baa92a27cef3477b17cb9e6
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-05-06 21:39:00 +0530

    Update Task_Scheduler_Script.sh

--- RealTime_Shell_Scripts/Task_Scheduler_Script.sh
+++ RealTime_Shell_Scripts/Task_Scheduler_Script.sh
@@ -1,6 +1,5 @@
-13. Task Scheduler Script:
 #!/bin/bash
-scheduled_task="/opt/cpuutil.sh"
+scheduled_task="/home/ec2-user/Linux-shellscripts-Notes/RealTime_Shell_Scripts/System_Information_Script.sh"
 schedule_time="0 2 * * *"
 # Schedule a task using cron
 echo "$schedule_time $scheduled_task" | crontab -
