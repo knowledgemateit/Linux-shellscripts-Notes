3ae5e6a17bbe271a36124c3a451dba1370d1fa7d
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-05-06 21:40:08 +0530

    Update Task_Scheduler_Script.sh

--- RealTime_Shell_Scripts/Task_Scheduler_Script.sh
+++ RealTime_Shell_Scripts/Task_Scheduler_Script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scheduled_task="/home/ec2-user/Linux-shellscripts-Notes/RealTime_Shell_Scripts/System_Information_Script.sh"
 schedule_time="0 2 * * *"
-# Schedule a task using cron
+# Schedule a task using cron   dnf install cronie -y  |  systemctl start crond | systemctl enable crond
 echo "$schedule_time $scheduled_task" | crontab -
 echo "Task scheduled successfully."
