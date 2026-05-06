670a1a6c12e9bbc6b80db77733df541b9cc2afd1
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-05-06 21:22:08 +0530

    Update File_Encryption-Decryption_Script.sh

--- RealTime_Shell_Scripts/File_Encryption-Decryption_Script.sh
+++ RealTime_Shell_Scripts/File_Encryption-Decryption_Script.sh
@@ -1,6 +1,6 @@
 6. File Encryption-Decryption Script:
 #!/bin/bash
-file="/path/to/file.txt"
+file="/opt/file.txt"
 # Encrypt file using AES-256-CBC
 openssl enc -aes-256-cbc -salt -in "$file" -out "$file.enc"
 echo "File encrypted: $file.enc"
