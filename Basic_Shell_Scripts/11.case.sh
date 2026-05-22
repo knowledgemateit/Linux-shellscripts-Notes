65c753306df5c00b52310ae0b1b31fd1a97fc09b
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-05-22 11:38:46 +0530

    Update 11.case.sh

--- Basic_Shell_Scripts/11.case.sh
+++ Basic_Shell_Scripts/11.case.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
-echo "Please talk to me ..."
-while :
-do
-  read INPUT_STRING
-  case $INPUT_STRING in
-	hello)
-		echo "Hello yourself!"
-		;;
-	bye)
-		echo "See you again!"
-		break
-		;;
-	*)
-		echo "Sorry, I don't understand"
-		;;
-  esac
-done
-echo 
-echo "That's all folks!"
+read $ENV
+case "$ENV" in
+    dev)
+        DB_SERVER="dev-db.local"
+        API_URL="https://dev-api.company.com"
+        ;;
+    uat)
+        DB_SERVER="uat-db.company.internal"
+        API_URL="https://uat-api.company.com"
+        ;;
+    prod)
+        DB_SERVER="prod-db-cluster.company.com"
+        API_URL="https://api.company.com"
+        ;;
+    *)
+        # The '*' acts like an "else". It catches any invalid inputs.
+        echo "Error: Invalid input! Use: $0 dev, uat, or prod"
+        exit 1
+        ;;
+esac
+
+# 3. Print the results
+echo "--- Running on environment: $ENV ---"
+echo "Database: $DB_SERVER"
+echo "API Endpoint: $API_URL"
